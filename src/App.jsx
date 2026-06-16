1d655d969ae0ee8e277da44f285426ddc7000205
amdadul haque <amdadulhaque601671@gmail.com>
2026-06-16 23:25:17 +0600

    route created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+
 function App() {
   return (
-    <div className="underline">
-      Facehook
-   </div>
+    <Routes>
+      <Route element={ <HomePage/>} path="/" exact/>
+      <Route element={ <RegisterPage/>} path="/register" />
+      <Route element={ <ProfilePage/>} path="/me" />
+      <Route element={<LoginPage />} path="/login" />
+      
+      <Route element={ <NotFoundPage/>} path="*" />
+    </Routes>
   )
 }
 
